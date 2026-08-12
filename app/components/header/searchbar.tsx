2ee7edb612201031c6da478f3a391b233d29dee5
bruno <brunopc1357@gmail.com>
2026-08-12 07:56:14 -0300

    ajuste da barra de pesquisa, unindo a ultima parte com a lupa

--- app/components/header/searchbar.tsx
+++ app/components/header/searchbar.tsx
@@ -14,28 +14,27 @@ export function SearchBar() {
         </div>
   
         <div className="h-8 w-px bg-gray-300"></div>
-  
-        <div className="flex flex-col px-20 py-3 cursor-pointer rounded-full hover:bg-gray-300">
-          <span className="text-sm font-bold text-gray-900">Quem</span>
-          <span className="text-base text-gray-500">Hóspedes?</span>
-        </div>
-  
-        <button className="flex items-center justify-center w-12 h-12 bg-red-500 rounded-full mr-2 hover:bg-red-600 transition">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-5 h-5 text-white"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={3}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 21l-4.35-4.35M17 11a6 6 0 11-12 0 6 6 0 0112 0z"
-            ></path>
-          </svg>
-        </button>
+        <div className="flex flex-col pl-18 pr-24 py-3 cursor-pointer hover:bg-gray-300 rounded-full transition">
+  <span className="text-sm font-bold text-gray-900">Quem</span>
+  <span className="text-base text-gray-500">Hóspedes?</span>
+</div>
+
+<button className="flex items-center justify-center w-14 h-14 bg-red-500 rounded-full hover:bg-red-600 transition -ml-14">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="w-5 h-5 text-white"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    strokeWidth={3}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M21 21l-4.35-4.35M17 11a6 6 0 11-12 0 6 6 0 0112 0z"
+    ></path>
+  </svg>
+</button>
       </div>
     );
   }
