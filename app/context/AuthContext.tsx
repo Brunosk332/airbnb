51f7817bee6e1fed9ba6b35e943c137d69ffaec7
bruno <brunopc1357@gmail.com>
2026-09-23 18:58:32 -0300

    corrigido bugs em apis de become-host e checkHost

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -48,56 +48,56 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     fetchUser();
   }, []);
 
-async function newProperty(formData: FormData): Promise<AuthResult> {
-  const res = await fetch("/api/newProperty", {
-    method: "POST",
-    body: formData,
-  });
+  async function newProperty(formData: FormData): Promise<AuthResult> {
+    const res = await fetch("/api/newProperty", {
+      method: "POST",
+      body: formData,
+    });
 
-  const data = await res.json();
+    const data = await res.json();
 
-  if (!res.ok) {
-    return { success: false, message: data.error };
+    if (!res.ok) {
+      return { success: false, message: data.error };
+    }
+    return { success: true };
   }
-  return {success: true};
-}
-//  async function newProperty(
-//    propertyType: string,
-//    location: string,
-//    pricePerNight: string,
-//    maxGuests: string,
-//    bedrooms: string,
-//    bathrooms: string,
-//    allowsPets: string,
-//    hasWifi: string,
-//    hasParking: string,
-//    description: string,
-//  ) : Promise<AuthResult> {
-//    const res = await fetch("/api/newProperty", {
-//      method: "POST",
-//      headers: { "Content-Type": "application/json" },
-//      body: JSON.stringify({
-//        type: propertyType,
-//        location,
-//        price_per_night: Number(pricePerNight),
-//        max_guests: Number(maxGuests),
-//        bedrooms: Number(bedrooms),
-//        bathrooms: Number(bathrooms),
-//        allows_pets: allowsPets === "true",
-//        has_wifi: hasWifi === "true",
-//        has_parking: hasParking === "true",
-//        description,
-//      }),
-//    });
-//  
-//    const data = await res.json();
-//  
-//    if (!res.ok) {
-//      return { success: false, message: data.error };
-//    }
-//  
-//    return { success: true };
-//  }
+  //  async function newProperty(
+  //    propertyType: string,
+  //    location: string,
+  //    pricePerNight: string,
+  //    maxGuests: string,
+  //    bedrooms: string,
+  //    bathrooms: string,
+  //    allowsPets: string,
+  //    hasWifi: string,
+  //    hasParking: string,
+  //    description: string,
+  //  ) : Promise<AuthResult> {
+  //    const res = await fetch("/api/newProperty", {
+  //      method: "POST",
+  //      headers: { "Content-Type": "application/json" },
+  //      body: JSON.stringify({
+  //        type: propertyType,
+  //        location,
+  //        price_per_night: Number(pricePerNight),
+  //        max_guests: Number(maxGuests),
+  //        bedrooms: Number(bedrooms),
+  //        bathrooms: Number(bathrooms),
+  //        allows_pets: allowsPets === "true",
+  //        has_wifi: hasWifi === "true",
+  //        has_parking: hasParking === "true",
+  //        description,
+  //      }),
+  //    });
+  //
+  //    const data = await res.json();
+  //
+  //    if (!res.ok) {
+  //      return { success: false, message: data.error };
+  //    }
+  //
+  //    return { success: true };
+  //  }
   // cria uma conta para o usuario
   async function register(
     name: string,
@@ -144,17 +144,19 @@ async function newProperty(formData: FormData): Promise<AuthResult> {
   }
   // sai da conta do usuario
   async function logout() {
-      const res = await fetch("/api/logout", {
- method: "POST",
-      })
-      if (!res.ok) {
-        console.error("Erro ao deletar sessão");
-      }  
-      setUser(null);
+    const res = await fetch("/api/logout", {
+      method: "POST",
+    });
+    if (!res.ok) {
+      console.error("Erro ao deletar sessão");
     }
+    setUser(null);
+  }
 
   return (
-    <AuthContext.Provider value={{ user, login, register, logout, newProperty }}>
+    <AuthContext.Provider
+      value={{ user, login, register, logout, newProperty }}
+    >
       {children}
     </AuthContext.Provider>
   );
